ebcd9c003399e67d1fe228d6a94547cba5b692df
raphaelmour <raphael.mourab@gmail.com>
2022-06-08 15:31:06 -0300

    arq do sistema de cards dinamico

---
+++ js/app.js
@@ -0,0 +1,54 @@
+const formulario = document.querySelector('form')
+const contas = document.querySelector('.container');
+const contasCriadas = []
+
+formulario.addEventListener('submit', function (event) {
+
+    const dados = Object.fromEntries(new FormData(formulario).entries())
+
+    adicionarCard(dados);
+    event.preventDefault();
+})
+
+
+
+function adicionarCard(dados) {
+
+
+    contasCriadas.push(dados);
+
+    contas.innerHTML = ""
+    contasCriadas.forEach((conta) => {
+        const card = document.createElement('div')
+        card.innerHTML = `<div class="card fundo">
+                         <div class="titulo">
+                             <h3>${conta.nome}</h3>
+                         </div>
+                         <div class="imagem">
+                                 <img src="${conta.link}" alt="">
+                             </div>
+                         <div class="descricao">
+                             <p>${conta.desc}</p>
+                         </div>`
+        card.setAttribute("class", "card");
+        contas.appendChild(card);
+        let primeiroPost = contas.firstChild;
+        contas.insertBefore(card, primeiroPost)
+
+    });
+}
+
+function removeCard() {
+    contasCriadas.pop()
+}
+
+
+
+let nome = prompt('Digite seu nome: ')
+let idade = parseInt(prompt('Digite sua idade: '))
+
+if (idade >= 18) {
+    alert(`${nome}, vocẽ é maior de idade!`)
+} else {
+    alert(`${nome}, você é menor de idade`)
+}
